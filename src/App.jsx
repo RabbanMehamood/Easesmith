03e436c707a5aa022e2110d25e6127647a20808a
RabbanMehamood <shaik.mehamood786@gmail.com>
2024-10-19 16:45:04 +0530

    Save Images Public

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import PlantCarousel from "./components/PlantCarousel";
+import ProductListSection from "./components/ProductListSection";
+import TopSection from "./components/TopSection";
 import Home from "./pages/Home";
+
 
 export default function App() {
   return (
     <>
       <Header />
+      <TopSection/>
       <Home />
+      <PlantCarousel/>
+      <ProductListSection/>
       <Footer />
     </>
   );
--- src/components/HeaderMain.jsx
+++ src/components/HeaderMain.jsx
@@ -1,6 +1,6 @@
 const HeaderMain = () => {
   return (
-    <header className="flex justify-between px-10 items-center py-3 mb-96">
+    <header className="flex justify-between px-10 items-center py-3">
       <div className="flex items-center">
         <img src="/images/chaperone_logo.png" alt="Logo" className="w-10" />
         <p className="font-semibold text-[#0f4811] ml-3">Chaperone</p>
@@ -16,9 +16,15 @@ const HeaderMain = () => {
           <li className="pl-8">FAQs</li>
         </ul>
       </nav>
-      <div className="font-medium">
-        <span>My Profile</span>
-        <span className="ml-2">Cart</span>
+      <div className="flex font-medium justify-items-center">
+        <div className="flex-row justify-center items-center">
+            <img src="/images/profile.256x256.png" className="w-5 h-5"/>
+            <span className="self-center">My Profile</span></div>
+        
+        <div>
+            <img src="/images/cart-image.png" className="w-5 h-5"/>
+            <span className="ml-2">Cart</span>
+            </div>
       </div>
     </header>
   );
